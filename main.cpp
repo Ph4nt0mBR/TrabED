92bf755c08a671bacd0bf182deaaf2ebb617b3f5
Afonso <afonsopalmeida15@gmail.com>
2025-05-14 15:41:26 +0100

    tudo funciona exceto importpass

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include<locale>
 #include "func.h"
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "pt_PT.UTF-8");
+    //calcDistancia();
+    pListasensor Ls = crialistasensores();
+    pListapassagem pp = crialistapasagem();
+    importsensor(Ls);
     char c[10] = "Renault";
 	pListadono Ld = crialistadono();
     pmarca m = criamarca(c);
@@ -10,6 +17,7 @@ int main() {
 	//regist_dono(Ld);
 	//list_dono(Ld);
     importcarro(Ld, m);
+    importpassagem(pp,m);
     //regist_veiculo(Ld,m);
     //list_veiculo(m);
 	return 0;
