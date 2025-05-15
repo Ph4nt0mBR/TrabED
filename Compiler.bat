64d7b6f35d75b70d290b62d4290c4ce2eaa13b43
Samuel <samgvo@hotmail.com>
2025-05-15 15:42:42 +0100

    Menu beta 1.0

---
+++ Compiler.bat
@@ -0,0 +1,2 @@
+gcc *.c -o trabED.exe
+pause
--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 #include <stdio.h>
 #include "func.h"
 
+
+
+/*    void exibirMenuPrincipal() {
+        printf("\n========== MENU PRINCIPAL ==========\n");
+        printf("1. Importar dados\n");
+        printf("2. Registrar novo dono\n");
+        printf("3. Registrar novo veiculo\n");
+        printf("4. Registrar passagem\n");
+        printf("5. Listar donos\n");
+        printf("6. Listar veiculos\n");
+        printf("7. Organizar donos\n");
+        printf("8. Organizar veiculos\n");
+        printf("9. Estatisticas de circulacao\n");
+        printf("10. Marca mais popular\n");
+        printf("11. Exportar dados\n");
+        printf("0. Sair\n");
+        printf("====================================\n");
+        printf("Escolha uma opcao: ");
+
+    }*/
+
+
+
 int main() {
     char c[10] = "Renault";
 	pListadono Ld = crialistadono();
@@ -12,5 +35,82 @@ int main() {
     importcarro(Ld, m);
     //regist_veiculo(Ld,m);
     //list_veiculo(m);
-	return 0;
-}
+
+/*
+        // Inicializa estruturas
+        Listadono* Ld = crialistadono();
+        marca* marcas = criamarca(""); // Inicialização simplificada
+        Listapassagem* Lp = crialistapasagem();
+
+        int opcao;
+        do {
+            exibirMenuPrincipal();
+            scanf("%d", &opcao);
+
+            switch (opcao) {
+            case 1: {
+                // Submenu de importação
+                printf("\n--- Importar Dados ---\n");
+                printf("1. Donos\n2. Carros\n3. Passagens\n4. Sensores\n");
+                int subop;
+                scanf("%d", &subop);
+                if (subop == 1) importdono(Ld);
+                else if (subop == 2) importcarro(Ld, marcas);
+                else if (subop == 3) importpassagem(Lp, marcas);
+                else if (subop == 4) importsensor(Ld);
+                break;
+            }
+            case 2:
+                regist_dono(Ld);
+                break;
+            case 3:
+                regist_veiculo(Ld, marcas);
+                break;
+            case 4:
+                regist_pass(Lp, marcas->inf, NULL); // Ajuste conforme sua implementação
+                break;
+            case 5:
+                list_dono(Ld);
+                break;
+            case 6:
+                list_veiculo(marcas);
+                break;
+            case 7:
+                organizadonos(Ld);
+                break;
+            case 8:
+                organizacarros(*marcas);
+                break;
+            case 9: {
+                // Submenu de estatísticas
+                printf("\n--- Estatisticas ---\n");
+                printf("1. Ranking por quilometragem\n2. Ranking por marca\n");
+                int subop;
+                scanf("%d", &subop);
+                if (subop == 1) rankveiculos(*Lp, calcDistancia());
+                else if (subop == 2) rankmarcas(*Lp, calcDistancia(), *marcas);
+                break;
+            }
+            case 10:
+                marcapopular(*marcas);
+                break;
+            case 11:
+                printf("\n--- Exportar ---\n");
+                printf("1. CSV\n2. XML\n");
+                int subop;
+                scanf("%d", &subop);
+                if (subop == 1) exportarXl();
+                else if (subop == 2) exportarXML();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+            }
+        } while (opcao != 0);
+        */
+        // Liberar memória (implemente conforme necessário)
+        return 0;
+    }
+
